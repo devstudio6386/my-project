f1b6978570d639ab1606db07d70ef6597b634a17
devstudio6386 <himanshusingh88pp@gmail.com>
2026-02-24 19:01:25 +0530

    update

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,11 @@
-const CACHE_NAME = "restaurant-cache-v1";
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll([
-        "./",
-        "./dashboard.html"
-      ]);
-    })
-  );
+self.addEventListener("install", () => {
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", () => {
+  // Do nothing – no caching
 });
